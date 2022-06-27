0f5531fb035ead246d5447dd36319b16493534c0
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-27 20:03:46 +0200

    Updated readiness and liveness probe. Fixed endpoint hosts/urls in values.yaml.

--- 5.0.0-HF2/minikube-setup.sh
+++ 5.0.0-HF2/minikube-setup.sh
@@ -1,6 +1,5 @@
-minikube start --memory 8192 --cpus 4 --driver=docker
-minikube addons enable ingress
+minikube delete --purge
+minikube start --memory 10692 --cpus 4 --driver=docker && minikube addons enable ingress
 eval $(minikube docker-env)
 docker images
-docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/fc:5.0.0-EB20211012
-docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/ignite:1.9.0-EB20210922
+docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/fc:5.0.0-HF2 && docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/ignite_p13:5.0.0-HF2

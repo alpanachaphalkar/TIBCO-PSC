688a6d28c0c4616bcf36eed83132fbec3f798300
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-29 20:09:09 +0200

    Updated minikube-setup.sh

--- 5.0.0-HF2/minikube-setup.sh
+++ 5.0.0-HF2/minikube-setup.sh
@@ -2,4 +2,5 @@ minikube delete --purge
 minikube start --memory 10692 --cpus 4 --driver=docker && minikube addons enable ingress
 eval $(minikube docker-env)
 docker images
+gcloud auth configure-docker
 docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/fc:5.0.0-HF2 && docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/psc/ignite_p13:5.0.0-HF2
